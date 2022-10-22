3f36dcead8acee8b17deaebcff11d084c6112ba3
samuelson <samuelsonowei04@gmail.com>
2022-10-22 15:18:32 +0100

    commited

---
+++ bin/da-work-guide/chspter-7-nullability/mini_exercise/mini_exercise.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  /* 
+  1. Create a String? variable called profession, 
+  but don’t give it a value. Then you’ll have 
+  profession null. :] 
+  */
+  String? profession;
+  /* 
+  2. Give profession a value of “basketball player" */
+  profession = 'Basketball Player';
+  print(profession);
+
+  /* 
+  3. Write the following line and then hover your 
+  cursor over the variable name. What type does 
+  Dart infer iLove to be? 
+  */
+
+  const iLove = 'Dart';
+  // Type dart infers is a String.
+}
